61b7d048497bb4f9b4022aa6b0660a1d30033bdb
Mateusz Grzonka <118964850+mateushhh@users.noreply.github.com>
2025-05-13 02:35:44 +0200

    Added more functionalities. Camera & Collision
    Smooth camera movement
    Collisions with working role change after contact
    Small changes in player code structure

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,30 +1,58 @@
 extends CharacterBody2D
+class_name Player
 
 @onready
 var sprite = $Sprite2D
+@onready
+var camera = $Camera2D
+@onready
+var collision_box = $CollisionShape2D
 
-enum {HUMAN, ZOMBIE}
-const VELOCITY = 100
-var current_role = ZOMBIE
+enum role {HUMAN, ZOMBIE}
+@export
+var current_role: role = role.HUMAN
+@export
+var playable: bool = false
+
+const VELOCITY = 64
 
 func set_sprite() -> void:
-	if current_role == HUMAN:
+	if current_role == role.HUMAN:
 		sprite.region_rect = Rect2(Vector2(0,0),Vector2(16,16))
-	elif current_role == ZOMBIE:
+	elif current_role == role.ZOMBIE:
 		sprite.region_rect = Rect2(Vector2(32,16),Vector2(16,16))
 
 func _ready() -> void:
+	if playable:
+		camera.enabled = true
 	set_sprite()
 
 func _process(delta: float) -> void:
 	# movement and inputs
-	velocity.x = Input.get_axis("ui_left", "ui_right")
-	velocity.y = Input.get_axis("ui_up", "ui_down")
-	velocity = velocity.normalized() * VELOCITY
-	move_and_slide()
+	if playable:
+		velocity.x = Input.get_axis("ui_left", "ui_right")
+		velocity.y = Input.get_axis("ui_up", "ui_down")
+		velocity = velocity.normalized() * VELOCITY
+		move_and_slide()
 	
 	# flip player horizontally
 	if velocity.x < 0:
 		sprite.flip_h = true
-	else: 
+	elif velocity.x > 0: 
 		sprite.flip_h = false
+		
+	# collisions
+	if get_last_slide_collision():
+		var collider = get_last_slide_collision().get_collider()
+		if collider.get_class() == "CharacterBody2D":
+			print("Collision with other Player")
+			var collider_instance : Player = instance_from_id(collider.get_instance_id())
+			# get infected
+			if collider_instance.current_role == role.ZOMBIE:
+				current_role = role.ZOMBIE
+			# infect other player or nothing happens
+			elif collider_instance.current_role == role.HUMAN:
+				if current_role == role.ZOMBIE:
+					collider_instance.current_role = role.ZOMBIE
+			set_sprite()
+			collider_instance.set_sprite()
